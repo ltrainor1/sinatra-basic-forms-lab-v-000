48f6ea01550190285dbf9077c35461a0d0c68f26
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-27 01:15:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,17 @@
 require_relative 'config/environment'
-require 'pry'
+
 class App < Sinatra::Base
 
-  get '/' do
-    erb :index
-  end
+  get '/' do 
+    erb :index 
+  end 
 
   get '/new' do
     erb :create_puppy
-  end
+  end 
 
-  post '/display_puppy' do
-    @puppy = Puppy.new(params[:name], params[:age], params[:breed])
-    binding.pry
-    erb :display_puppy
-  end
+  post '/display_puppy' do 
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+  end 
 
 end
