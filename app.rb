5ff8140c46169c38b49d9c0dbd86072706919c68
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-27 01:18:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,16 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    erb :index 
-  end 
+  get '/' do
+    erb :index
+  end
 
   get '/new' do
     erb :create_puppy
-  end 
+  end
 
-  post '/display_puppy' do 
+  post '/display_puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
-  end 
+  end
 
 end
